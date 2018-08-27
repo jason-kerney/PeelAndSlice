788749f13533454e722076dfd2239fb2d3c8f844
Jason Kerney <jason.kerney@hunterindustries.com>
2018-08-27 15:40:04 -0700

    cleaned up and removed irrelivant confusion

--- EcmaScript/program/app/quiz/framerate.js
+++ EcmaScript/program/app/quiz/framerate.js
@@ -1,28 +1,40 @@
-const ntscFrameRate = []
+const ntscFrameRate = 33;
 
-let value = (function () {
-    function getCreateCountdownFrames (frameRate) {
-        return function (http, videoMaker) {
-            console.log(frameRate);
-            const type = http.request.params["TvFormat"];
-            let frames = 0;
+function getCreateCountdownFrames(frameRate) {
+    return function (http, videoMaker) {
+        console.log(frameRate);
+        const type = http.request.params["TvFormat"];
+        let frames = 0;
 
-            if ("ntsc" === type) {
-                frames = frameRate + ['end frame'];
-            }
+        if ("ntsc" === type) {
+            frames = (new TvType(type, ntscFrameRate)).getFrameRate();
+        }
 
-            for (let i = 0; i < frames.length; i++) {
-                videoMaker.createFrame(frames[i]);
-            }
+        for (let i = 0; i < frames; i++) {
+            videoMaker.createFrame(i);
         }
     }
+}
 
-    let obj = {
-        createCountdownFrames: getCreateCountdownFrames((frameRate = ntscFrameRate, frameRate + 2, frameRate))
-    };
+function TvType(tvType, frameRate) {
+    BASE_FRAMERATE = 0;
+    this.frameRate = frameRate;
+}
 
-    return obj;
-})();
+TvType.prototype = {
+    getFrameRate: function () {
+        if (tvType === 'ntsc') {
+            if (this.frameRate + this.BASE_FRAMERATE > 0) {
+                return this.frameRate + this.BASE_FRAMERATE;
+            } else {
+                return 42;
+            }
+        } else {
+            return -1;
+        }
+    }
+};
 
-
-module.exports = value;
+module.exports = {
+    createCountdownFrames: getCreateCountdownFrames(ntscFrameRate)
+};
